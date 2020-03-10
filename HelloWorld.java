bdfeeae10ce90aed53fef82544a64bfc84037b4c
vaishnavi-bale <59390707+vaishnavi-bale@users.noreply.github.com>
2020-03-10 11:45:16 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+class HelloWorld{
+public static void main(String args[]){
+
+     LocalDate date=LocalDate.now();
+     System.out.println("today's date is :"+date);
+}
+}
